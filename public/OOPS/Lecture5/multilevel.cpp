a9c61cd5906cf97b7a1599174356591ae54db37c
{Bhanu <partapbhanu516@gmail.com>
2026-04-14 19:29:56 +0530

    levels of inheritance 1

---
+++ public/OOPS/Lecture5/multilevel.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+
+class Person{
+    protected:
+    string name;
+    
+    public:
+    void work(){
+        cout<<"Welcome to the work class"<<endl;
+    }
+};
+class Employee: public Person{
+    protected:
+        int salary;
+    public:
+        void month_salary(){
+            cout<<"This is my salary"<<salary<<endl;
+        }
+};
+class Manager: public Employee{
+    public:
+    string promotion;
+    Manager(string name, int salary, string promotion){
+        this->name = name;
+        this->salary = salary;
+        this->promotion = promotion;
+    }
+    void result(){
+        cout<<promotion<<"You have been promoted to "<<salary<<endl;
+    }
+};
+
+int main(){
+    Manager M1("Bhanu", 1000000, "Yes");
+    M1.result();
+    M1.month_salary();
+    M1.work();
+}
