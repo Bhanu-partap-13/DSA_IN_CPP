b1085b78361d7426d9bb95f66ebf7fb8212038ff
{Bhanu <partapbhanu516@gmail.com>
2026-04-20 19:06:10 +0530

    exception handling anothr eg

--- public/OOPS/Lecture7/exception.cpp
+++ public/OOPS/Lecture7/exception.cpp
@@ -49,20 +49,56 @@ int main(){
     return 0;
 }
 
+//Another example of exception handling
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//     try{
+//         int a, b;
+//         cin>>a>>b;
+//         if(b==0)    throw "We cannot divide a number by zero";
+//         cout<<a/b;
+//     }
+//     catch(const char *e)
+//     {
+//         cout<<"Exception occur: "<<e<<endl;
+//     }
+//     return 0;
+// }
+
+//if else or try main kya difference h
+//allocation of memory
+
 
 #include<iostream>
+#include<exception>
 using namespace std;
 
+class exception{
+    protected:
+    string msg;
+    public:
+    exception(string msg) this-> msg = msg;
+    string what(){
+        return msg;
+    }
+};
 int main(){
+    //int *ptr = new int[1000000000000];
+    // if(ptr == nullptr){
+    //     cout<<"Memory allocation failed\n";
+    // }
+    // else{
+    //     cout<<"Memory allocated successfully\n";
+    // }
+    // if we use if,and else then we will not be able to handle th exception..
     try{
-        int a, b;
-        cin>>a>>b;
-        if(b==0)    throw "We cannot divide a number by zero";
-        cout<<a/b;
+        int *ptr = new int[1000000000000];
+        throw "Memory allocation success\n";
+        delete []ptr;
     }
-    catch(const char *e)
-    {
-        cout<<"Exception occur: "<<e<<endl;
+    catch(const exception &e){
+        cout<<"Exception occur due to line: "<<e.what()<<endl; 
     }
-    return 0;
 }
