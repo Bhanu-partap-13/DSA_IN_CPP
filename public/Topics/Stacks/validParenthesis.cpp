cace9b68121c4c9a35d44d2f19edbb57b2e69cfc
{Bhanu <partapbhanu516@gmail.com>
2026-04-23 18:40:50 +0530

    leetcode valid parenthesis

--- public/Topics/Stacks/validParenthesis.cpp
+++ public/Topics/Stacks/validParenthesis.cpp
@@ -1,21 +1,60 @@
+// #include<iostream>
+// using namespace std;
+
+// bool isValid(string s){
+//     stack<int>st;
+//     int n = s.size();
+//     for(int i=0; i<n; i++){
+//         if(s[i] == '('){
+//             st.push(i);
+//         }
+//         else{
+//             if(st.size() == 0) return false;
+//             else st.pop();
+//         }
+//     }
+//     if(st.size() == 0) return true;
+//     else return false;
+// }
+
+// int main(){
+//     string s;
+//     cin>>s;
+//     if(isValid(s)){
+//         cout<<"Valid";
+//     }
+//     else{
+//         cout<<"Invalid";
+//     }
+//     return 0;
+// }
+
+//Multiple braces
+
 #include<iostream>
 using namespace std;
 
 bool isValid(string s){
-    stack<int>st;
+    stack<int> st;
     int n = s.size();
+
     for(int i=0; i<n; i++){
-        if(s[i] == '('){
+        if(s[i]=='(' || s[i]=='[' || s[i]=='{'){
             st.push(i);
         }
         else{
-            if(st.size() == 0) return false;
-            else st.pop();
+            if(st.size()== 0) return false;
+            else((st.top() == '(' &&  s[i] == ')') || (st.top() == '[' &&  s[i] == ']') || 
+            (st.top() == '{' &&  s[i] == '}')) st.pop();
+        }
+        else{
+            return false;
         }
     }
     if(st.size() == 0) return true;
     else return false;
 }
+
 
 int main(){
     string s;
