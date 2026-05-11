23d907703c7b4da8ccdb86ae7cae11001e7343cc
{Bhanu <partapbhanu516@gmail.com>
2026-05-11 20:00:19 +0530

    leetcode 53 kadane algorithm

--- public/Leetcode/53.cpp
+++ public/Leetcode/53.cpp
@@ -1,27 +1,3 @@
-// #include<iostream>
-// #include<vector>
-// #include<climits>
-// using namespace std;
-
-// class Solution {
-// public:
-//     int maxSubArray(vector<int>& nums) {
-//         int n = nums.size();
-//         int maxi = INT_MIN;
-
-//         for(int i=0; i<n; i++){
-//             int sum  = 0;
-//             for(int j=i; j<n; j++){
-//                 sum += nums[j];
-//                 maxi = max(sum , maxi);
-//             }
-//         }
-//     return maxi;
-//     }
-// };
-
-//but what if we want to return the subarray as well ?
-
 #include<iostream>
 #include<vector>
 #include<climits>
@@ -29,29 +5,52 @@ using namespace std;
 
 class Solution {
 public:
-    vector<vector<int>> maxSubArray(vector<int>& nums) {
+    int maxSubArray(vector<int>& nums) {
         int n = nums.size();
         int maxi = INT_MIN;
-        int start = 0;
-        int end = 0;
-        int tempStart = 0;
-        int sum = 0;
 
+        int sum  = 0;
         for(int i=0; i<n; i++){
-            if(sum < 0) {
-                sum = 0;
-                tempStart = i;
-            }
-            if(sum > maxi){
-                maxi = sum;
-                start = tempStart;
-                end = i;
-            }
-            vector<int> ans;
-            for(int i=start; i<=end; i++){
-                ans.push_back(nums[i]);
-            }
+        if(sum<0)sum=0;
+        sum+=nums[i];
+        maxi = max(sum, maxi);
         }
-        return ans;
+    return maxi;
     }
 };
+
+//but what if we want to return the subarray as well ?
+
+// #include<iostream>
+// #include<vector>
+// #include<climits>
+// using namespace std;
+
+// class Solution {
+// public:
+//     vector<int> maxSubArray(vector<int>& nums) {
+//         int n = nums.size();
+//         int maxi = INT_MIN;
+//         int start = 0;
+//         int end = 0;
+//         int tempStart = 0;
+//         int sum = 0;
+
+//         for(int i=0; i<n; i++){
+//             if(sum < 0) {
+//                 sum = 0;
+//                 tempStart = i;
+//             }
+//             if(sum > maxi){
+//                 maxi = sum;
+//                 start = tempStart;
+//                 end = i;
+//             }
+//             vector<int> ans;
+//             for(int i=start; i<=end; i++){
+//                 ans.push_back(nums[i]);
+//             }
+//         }
+//         return ans;
+//     }
+// };
