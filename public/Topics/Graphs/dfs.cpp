c4378648c5c3107620196bb77138698080899c78
{Bhanu <partapbhanu516@gmail.com>
2026-05-30 15:44:50 +0530

    graph DFS

--- public/Topics/Graphs/dfs.cpp
+++ public/Topics/Graphs/dfs.cpp
@@ -1,6 +1,17 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+void dfs(vector<vector<int>>& graph, int node, vector<bool>& visited, vector<int>& res){
+   if(visited[node]) return;
+    visited[node] = true;
+    res.push_back(node);
+
+    for(int neighbor : graph[node]){
+        if(!visited[neighbor]){
+            dfs(graph, neighbor, visited, res);
+        }
+    }
+}
 int main(){
     int n, m;
     cin >> n >> m;
@@ -19,7 +30,18 @@ int main(){
 
     for(int u = 0; u < n; u++){
         for(int v= graph[u].begin(); v != graph[u].end(); v++){
-            
+            adj[u].push_back(*v);
         }
     }
+    vector<int> res;
+    vector<bool> visited(n, false);
+
+    dfs(graph, 0, visited, res);
+    
+    cout << "DFS Traversal starting from node 0:" << endl;
+    for(int node : res){
+        cout << node << " ";
+    }
+    cout << endl;
+    return 0;
 }
