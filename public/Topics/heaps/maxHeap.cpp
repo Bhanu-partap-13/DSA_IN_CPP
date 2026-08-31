4a50eaffa42e24446439febef53a77c6f202ef65
{Bhanu <partapbhanu516@gmail.com>
2026-08-31 21:49:23 +0530

    leetcode 215, the question is of min heap, it is same as max heap for kth largest elements

--- public/Topics/heaps/maxHeap.cpp
+++ public/Topics/heaps/maxHeap.cpp
@@ -2,7 +2,8 @@
 using namespace std;
 
 int main(){
-    priority_queue<int> maxHeap; // This is a max heap ye default main bnta h
+    //max heap
+    priority_queue<int> pq; // This is a max heap ye default main bnta h
     pq.push(10);
     pq.push(110);
     pq.push(180);
@@ -10,4 +11,12 @@ int main(){
     cout << pq.top() << endl; // isme 180 ayega
     pq.pop(); // ab 180 ud jayega
     cout << pq.top() << endl; // ab 110 ayega
+
+    priority_queue<int, vector<int>, greater<int>> minHeap;
+
 }
+
+//why?
+/*
+In this DS if we want min. and max. at any point of time, then we use 
+*/
